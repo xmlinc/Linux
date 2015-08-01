10c93076cf4da3a38112a4db267a10fd555157eb
xmlinc <support@softinc.de>
2015-08-01 20:45:26 +0200

    wget test

--- elementaryOS/README.md
+++ elementaryOS/README.md
@@ -1,5 +1,4 @@
 # Pro installer 
 
-    curl https://github.com/xmlinc/Linux/raw/master/elementaryOS/installPro.sh | sh
-
-
+    #    curl https://github.com/xmlinc/Linux/raw/master/elementaryOS/installPro.sh | sh
+    wget -O ~/installPro.sh https://raw.githubusercontent.com/xmlinc/Linux/elementaryOS/installPro.sh | sh
--- elementaryOS/installPro.sh
+++ elementaryOS/installPro.sh
@@ -3,17 +3,31 @@
 cd ~/
 
 #First you update your system
-sudo apt-get update && sudo apt-get dist-upgrade
+sudo -y apt-get update
+sudo -y apt-get dist-upgrade
 
 # install php mysql apache webserver
 # sudo apt-get install apache2 mysql-server mysql-client php5 phpmyadmin
 
 # sudo apt-get install skype
-mkdir upgrade
+mkdir Upgrade
+cd ~/Upgrade
 
 sudo apt-get install virtualbox-guest-dkms
 #sudo apt-get install virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
 
 # Show listed all installed Packets on this System
-cat /etc/apt/sources.list /etc/apt/sources.list.d/*.list  > sources.save.list
+cat /etc/apt/sources.list  > sources.save.list
+cat /etc/apt/sources.list.d/*.list  >> sources.save.list
 mv sources.save.list -/sources.save.txt
+
+if [ ! -e VBoxGuestAdditions_5.0.0.iso ]; then
+	wget -O VBoxGuestAdditions_5.0.0.iso http://download.virtualbox.org/virtualbox/5.0.0/VBoxGuestAdditions_5.0.0.iso
+fi
+
+sudo mkdir /mnt/VBGA/
+sudo mount VBoxGuestAdditions_5.0.0.iso /mnt/VBGA/
+#umount
+cd /mnt/VBGA/
+sudo ./VBoxLinuxAdditions.run
+
