7c2810cede2c507e838944893b099093318db703
xmlinc <support@softinc.de>
2015-08-01 22:56:33 +0200

    mount guest additions for fullscreen mode

--- elementaryOS/installPro.sh
+++ elementaryOS/installPro.sh
@@ -4,22 +4,26 @@ cd ~/
 
 #First you update your system
 sudo -y apt-get update
-sudo -y apt-get dist-upgrade
+#sudo -y apt-get dist-upgrade
+sudo -y apt-get upgrade
 
 # install php mysql apache webserver
 # sudo apt-get install apache2 mysql-server mysql-client php5 phpmyadmin
+
+# Install additional codecs to run all sorts of media
+sudo -y apt-get install ubuntu-restricted-extras
 
 # sudo apt-get install skype
 mkdir Upgrade
 cd ~/Upgrade
 
-sudo apt-get install virtualbox-guest-dkms
-#sudo apt-get install virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
+#sudo -y apt-get install virtualbox-guest-dkms
+sudo -y apt-get install virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
 
 # Show listed all installed Packets on this System
 cat /etc/apt/sources.list  > sources.save.list
 cat /etc/apt/sources.list.d/*.list  >> sources.save.list
-mv sources.save.list -/sources.save.txt
+#mv sources.save.list ~/sources.save.txt
 
 if [ ! -e VBoxGuestAdditions_5.0.0.iso ]; then
 	wget -O VBoxGuestAdditions_5.0.0.iso http://download.virtualbox.org/virtualbox/5.0.0/VBoxGuestAdditions_5.0.0.iso
